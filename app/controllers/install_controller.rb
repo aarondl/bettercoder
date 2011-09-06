f9ff793fe3c32029a1e82906eb5b73c76a9e7081
Aaron L <aaron@bettercoder.net>
2011-09-06 01:14:49 -0700

    Green: Installation.feature
    - Adds an installation controller.
    - Amends routes.
    - Adds cucumber path for installation.

---
+++ app/controllers/install_controller.rb
@@ -0,0 +1,10 @@
+class InstallController < ActionController::Base
+  protect_from_forgery
+
+  def index
+  end
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Bettercoder::Application.routes.draw do
-  get "site/index"
+  get 'site/index'
+
+  match 'install' => 'install#new', :via => :get
+  match 'install' => 'install#index', :via => :post
+  get 'install/new'
 
   root :to => 'site#index'
 end
